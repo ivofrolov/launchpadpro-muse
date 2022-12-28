a7f6e640a31cd4ac0ee685b434d7ac8d938ac3d3
Ivan Frolov <ivofrolov@fastmail.com>
2022-12-28 14:39:42 +0400

    Fixes tests assertions types

--- test/test_muse/test_utils.c
+++ test/test_muse/test_utils.c
@@ -13,27 +13,27 @@ void tearDown(void)
 
 void test_four_bit_parity()
 {
-  TEST_ASSERT_EQUAL(1, four_bit_parity(0));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(1));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(2));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(3));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(4));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(5));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(6));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(7));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(8));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(9));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(10));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(11));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(12));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(13));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(14));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(15));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(0));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(1));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(2));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(3));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(4));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(5));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(6));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(7));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(8));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(9));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(10));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(11));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(12));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(13));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(14));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(15));
   // more than 4 bits input
-  TEST_ASSERT_EQUAL(1, four_bit_parity(16));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(17));
-  TEST_ASSERT_EQUAL(0, four_bit_parity(18));
-  TEST_ASSERT_EQUAL(1, four_bit_parity(19));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(16));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(17));
+  TEST_ASSERT_EQUAL_UINT8(0, four_bit_parity(18));
+  TEST_ASSERT_EQUAL_UINT8(1, four_bit_parity(19));
 }
 
 int main( int argc, char **argv)
